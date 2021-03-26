dcf09553c7b781d825a16d8fb160857f6e4610a9
Federico <federicofigini90@gmail.com>
2021-03-26 17:17:57 +0100

    layout ok

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: "#root",
+    data: {
+        pippo: contacts
+    },
+    methods: {
+
+    },
+    mounted: function() {
+        console.log(this.pippo);
+    }
+
+
+})
