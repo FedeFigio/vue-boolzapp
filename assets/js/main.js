0fe33a786be91ee50676f77d5ea499ce7e8e16f3
Federico <federicofigini90@gmail.com>
2021-03-27 11:50:34 +0100

    mileston 3 ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ var app = new Vue({
     el: "#root",
     data: {
         contacts: contacts,
-        activeIndex: 0
+        activeIndex: 0,
+        inputTxt: "",
+
     },
     methods: {
         setActiveContact(index) {
@@ -11,15 +13,60 @@ var app = new Vue({
         lastMessage(index) {
             var msgs = contacts[index].messages
             return msgs[msgs.length - 1]
+        },
+
+        autoAnswer() {
+            setTimeout(() => {
+                let obj = {
+                    date: this.actualDate,
+                    text: "ok",
+                    status: 'received'
+                }
+                contacts[this.activeIndex].messages.push(obj)
+
+            }, 1000);
+
+        },
+        sentMessage() {
+            if (this.inputTxt != "") {
+
+                let obj = {
+                    date: this.actualDate,
+                    text: this.inputTxt,
+                    status: 'sent'
+                }
+                console.log(obj.date);
+                contacts[this.activeIndex].messages.push(obj)
+                this.inputTxt = ""
+                this.autoAnswer()
+
+            }
         }
     },
     computed: {
         activeContactMessages() {
             return contacts[this.activeIndex].messages
+        },
+        actualDate() {
+            var today = new Date();
+
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+            var h = String(today.getHours())
+            var m = String(today.getMinutes())
+            var s = String(today.getSeconds())
+
+            today = `${mm}/${dd}/${yyyy} ${h}:${m}:${s}`
+
+            return today
         }
+
 
     },
     mounted() {
+
+
 
     }
 
