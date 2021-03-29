d9e6dd0f9a2f20208905be176d3edcf6f57fbeb2
Federico <federicofigini90@gmail.com>
2021-03-29 15:23:43 +0200

    wp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 var app = new Vue({
     el: "#root",
-    data: {
-        contacts: contacts,
-        activeIndex: 0,
-        inputTxt: "",
 
+    data() {
+        return {
+            contacts: contacts,
+            activeIndex: 0,
+            inputTxt: "",
+            inputSearch: "",
+        };
     },
     methods: {
         setActiveContact(index) {
@@ -14,42 +17,33 @@ var app = new Vue({
             var msgs = contacts[index].messages
             return msgs[msgs.length - 1]
         },
-
         autoAnswer() {
             setTimeout(() => {
                 let obj = {
-                    date: this.actualDate,
+                    date: this.actualDateMessage(),
                     text: "ok",
                     status: 'received'
                 }
                 contacts[this.activeIndex].messages.push(obj)
 
             }, 1000);
-
         },
         sentMessage() {
             if (this.inputTxt != "") {
 
                 let obj = {
-                    date: this.actualDate,
+                    date: this.actualDateMessage(),
                     text: this.inputTxt,
                     status: 'sent'
                 }
-                console.log(obj.date);
                 contacts[this.activeIndex].messages.push(obj)
                 this.inputTxt = ""
                 this.autoAnswer()
-
             }
-        }
-    },
-    computed: {
-        activeContactMessages() {
-            return contacts[this.activeIndex].messages
         },
-        actualDate() {
-            var today = new Date();
+        actualDateMessage() {
 
+            var today = new Date();
             var dd = String(today.getDate()).padStart(2, '0');
             var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
             var yyyy = today.getFullYear();
@@ -57,18 +51,32 @@ var app = new Vue({
             var m = String(today.getMinutes())
             var s = String(today.getSeconds())
 
-            today = `${mm}/${dd}/${yyyy} ${h}:${m}:${s}`
-
-            return today
+            return `${mm}/${dd}/${yyyy} ${h}:${m}:${s}`
+        },
+        toggleMenu(e) {
+            let editMessage = e.target.parentNode.querySelector(".edit-message")
+            let status = editMessage.classList.contains("active")
+            document.querySelectorAll(".edit-message").forEach((e) => {
+                e.classList.remove("active")
+            })
+            if (!status) {
+                editMessage.classList.add("active")
+            }
         }
-
-
     },
-    mounted() {
+    computed: {
+        activeContactMessages() {
+            return contacts[this.activeIndex].messages
+        },
+        filteredContacts() {
+            if (this.inputSearch == "" || this.inputSearch == null) {
+                return this.contacts;
+            }
+            return this.contacts.filter((contact) => {
+                return contact.name.toLowerCase().includes(this.inputSearch.toLowerCase());
 
-
-
-    }
-
-
+            })
+        },
+    },
+    mounted() {}
 })
